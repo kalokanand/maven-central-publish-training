1b72e8d738a6a1ebccf8aa70b1f7985964d6daf9
kalokanand <alok4java@gmail.com>
2020-07-17 13:54:15 +0530

    init commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# maven-central-publish-training
-maven-central-publish-training
---
+++ src/main/java/com/github/kalokanand/sample/Test.java
@@ -0,0 +1,9 @@
+package com.github.kalokanand.sample;
+
+public class Test {
+
+	public boolean isPublish() {
+		return true;
+	}
+
+}
